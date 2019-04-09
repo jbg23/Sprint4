10c7e2d316e9a07aa0900aa13ddcd0e1f9b065dd
Hrafnhildur Arna Nielsen <han7@hi.is>
2019-04-09 18:50:36 +0000

    Spurningaborð með kommentum

--- test_bord3.py
+++ test_bord3.py
@@ -1,12 +1,14 @@
-from pacman import Eltingaleikur
+#from pacman import Eltingaleikur
 #from pusluspil import Pusluspil
+from Spurningaleikur_grafik_rett import Question
 
 #__main_
-
+"""
 bord4=Eltingaleikur(1)
 bord4.pacIntro()
 bord4.byrja()
 bord4.pacSigur()
+"""
 
 """
 bord3 = Pusluspil(1)
@@ -17,3 +19,7 @@ bord3.pusluspilrun()
 bord3 = Pusluspil(1)
 bord3.puslSigur()
 """
+
+naesta=Question(1)
+naesta.spurningaIntro()
+naesta.gameLoop()

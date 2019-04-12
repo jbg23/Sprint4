7dd7e4bbc946145c41143127016d5dbe59d19fd0
Jóhanna Blöndahl <jbg23@hi.is>
2019-04-12 14:38:38 +0000

    sprint 4

--- test_bord1.py
+++
@@ -1,4 +0,0 @@
-from Spurningaleikur_grafik_rett import Question
-
-bord1 = Question(1,1)
-bord1.spurningaIntro()
--- test_bord4.py
+++
@@ -1,4 +0,0 @@
-from ofurmus import SuperMario
-
-bord4 = SuperMario(1,1)
-bord4.byrja()
